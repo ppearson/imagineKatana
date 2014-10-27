a898cbf136290446c4ebfba8cdf9590a778e2e79
Peter Pearson <peter.pearson@gmail.com>
2014-10-27 08:23:45 +0000

    Added missing file from previous commit.

---
+++ src/imagineRender/misc_helpers.h
@@ -0,0 +1,22 @@
+#ifndef MISC_HELPERS_H
+#define MISC_HELPERS_H
+
+struct CreationSettings
+{
+	CreationSettings() : m_applyMaterials(true), m_useTextures(true), m_enableSubdivision(false), m_deduplicateVertexNormals(false),
+		m_specialiseAssemblies(false), m_flipT(0), m_triangleType(0), m_specialisedTriangleType(0)
+	{
+	}
+
+	bool				m_applyMaterials;
+	bool				m_useTextures;
+	bool				m_enableSubdivision;
+	bool				m_deduplicateVertexNormals;
+	bool				m_specialiseAssemblies;
+
+	unsigned int		m_flipT;
+	unsigned int		m_triangleType;
+	unsigned int		m_specialisedTriangleType;
+};
+
+#endif // MISC_HELPERS_H
